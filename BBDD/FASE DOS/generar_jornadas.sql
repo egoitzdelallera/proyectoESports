7da3fc416dd6aa7e9539de12ba6a54ddcbdb5bab
Egoitz De la Llera <egoitz.delallera@ikasle.egibide.org>
2024-05-08 12:03:16 +0200

    No funciona el id_jornada,
    No consigo hacerlo, asi se queda.

--- BBDD/FASE DOS/generar_jornadas.sql
+++ BBDD/FASE DOS/generar_jornadas.sql
@@ -1,13 +1,17 @@
+-- La automatizacion de crear jornadas.
+
+
+-- Los enfrentamientos los crea todos, en la misma jornada, por mas que intento no consigo hacer que cambien de jornada.
 CREATE OR REPLACE PROCEDURE crear_jornadas_competicion 
     (p_id_competicion IN COMPETICIONES.ID_COMPETICION%TYPE)
 AS
     v_total_equipos INTEGER;
     v_total_jornadas INTEGER;
     v_enfrentamientos_por_jornada INTEGER;
-    v_jornada INTEGER := 1;
+    v_jornada INTEGER;
     v_enfrentamiento_existe INTEGER;
 BEGIN
-    -- Contar el total de equipos inscritos en la competición
+    -- Contar los equipos
     SELECT COUNT(*) INTO v_total_equipos
     FROM PARTICIPACIONES
     WHERE ID_COMPETICION = p_id_competicion;
@@ -18,10 +22,13 @@ BEGIN
     -- Calcular el total de enfrentamientos por jornada
     v_enfrentamientos_por_jornada := CEIL(v_total_equipos / 2);
 
+    -- Crear jornadas
     FOR i IN 1..v_total_jornadas LOOP
         -- Crear una nueva jornada
         INSERT INTO JORNADAS (ID_JORNADA, N_JORNADA, FECHA_JORNADA, ID_COMPETICION)
         VALUES (JORNADAS_SEQ.NEXTVAL, i, SYSDATE + i, p_id_competicion);
+        
+        v_jornada := JORNADAS_SEQ.CURRVAL;
         
         -- Obtener los equipos para esta jornada
         FOR e1 IN (SELECT ID_EQUIPO
@@ -41,10 +48,16 @@ BEGIN
                 IF v_enfrentamiento_existe = 0 THEN
                     INSERT INTO ENFRENTAMIENTOS (ID_ENFRENTAMIENTO, HORA, ID_JORNADA, ID_EQUIPO_LOCAL, ID_EQUIPO_VISITANTE)
                     VALUES (ENFRENTAMIENTOS_SEQ.NEXTVAL, SYSTIMESTAMP, v_jornada, e1.ID_EQUIPO, e2.ID_EQUIPO);
+                    
+                    -- Salir del bucle si se alcanza el número de enfrentamientos por jornada
+                    IF SQL%ROWCOUNT = v_enfrentamientos_por_jornada THEN
+                        EXIT;
+                    END IF;
                 END IF;
             END LOOP;
+            -- Salir del bucle externo si se alcanza el número de enfrentamientos por jornada
+            EXIT WHEN SQL%ROWCOUNT = v_enfrentamientos_por_jornada;
         END LOOP;
-        v_jornada := v_jornada + 1;
     END LOOP;
     
     DBMS_OUTPUT.PUT_LINE('Jornadas creadas para la competición ' || p_id_competicion || '.');
@@ -53,6 +66,8 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('Error al crear las jornadas: ' || SQLERRM);
 END;
 /
+
+
 
 
 CREATE OR REPLACE TRIGGER cambiar_estado_competicion
