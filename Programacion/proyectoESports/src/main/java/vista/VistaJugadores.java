1c97023677694c05b727c236d44d43f6f310744d
gorkaemaldi <gorka.emaldi@ikasle.egibide.org>
2024-05-14 13:16:48 +0200

    LDM jornadas y ultima_jornada

---
+++ Programacion/proyectoESports/src/main/java/vista/VistaJugadores.java
@@ -0,0 +1,10 @@
+package vista;
+
+import javax.swing.*;
+
+public class VistaJugadores {
+    private JComboBox comboBox1;
+    private JTextArea textArea1;
+    private JButton editarButton;
+    private JButton borrarButton;
+}
